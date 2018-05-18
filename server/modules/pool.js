49e2c23803046c5c8117323088b657577632d699
Melody Massard <melody.massard8988@gmail.com>
2018-05-18 13:02:19 -0500

    Database name set

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,7 +1,5 @@
-/* the only line you likely need to change is
-
- DATABASE NAME: 'reveAcademy',
-
+/*
+OUR DATABASE NAME: 'reveAcademy'
 */
 
 const pg = require('pg');
@@ -29,7 +27,7 @@ if (process.env.DATABASE_URL) {
   config = {
     host: 'localhost', // Server hosting the postgres database
     port: 5432, // env var: PGPORT
-    database: 'reveAcademy', 
+    database: 'reveAcademy', // CHANGE THIS LINE! env var: PGDATABASE, this is likely the one thing you need to change to get up and running
     max: 10, // max number of clients in the pool
     idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
   };
