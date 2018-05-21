6d1cf2249f19e82a547165646d2da9e0bc6a65f7
Tenzin Chosang <TenzinChosang@Tenzins-MacBook-Air.local>
2018-05-21 11:20:55 -0500

    fixed

--- src/redux/sagas/manageAccountsSaga.js
+++ src/redux/sagas/manageAccountsSaga.js
@@ -3,6 +3,20 @@ import axios from 'axios';
 
 function* manageAccountsSaga(){
     
+    
+}
+
+function* getAccountSaga(action){
+    try{
+        const accountResponse = yield call(axios.get, `/api/manage/${action.payload.name}`)
+        console.log('action payload of getProgramSaga', action.payload);
+        yield put({
+            type:'SET_PROGRAM_REDUCER',
+            payload: programResponse.data,
+        })
+    } catch(error){
+        console.log('get program error: ', error);
+    }
 }
 
 export default manageAccountsSaga;
