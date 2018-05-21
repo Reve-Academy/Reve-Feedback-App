621c925301569e84868e10b3cc3ea65110e076ca
Tenzin Chosang <TenzinChosang@Tenzins-MacBook-Air.local>
2018-05-21 11:23:33 -0500

    fixed

--- src/redux/sagas/manageAccountsSaga.js
+++ src/redux/sagas/manageAccountsSaga.js
@@ -1,22 +1,22 @@
-import { call, put, takeEvery } from 'redux-saga/effects';
-import axios from 'axios';
+// import { call, put, takeEvery } from 'redux-saga/effects';
+// import axios from 'axios';
 
-function* manageAccountsSaga(){
+// function* manageAccountsSaga(){
+
     
-    
-}
+// }
 
-function* getAccountSaga(action){
-    try{
-        const accountResponse = yield call(axios.get, `/api/manage/${action.payload.name}`)
-        console.log('action payload of getProgramSaga', action.payload);
-        yield put({
-            type:'SET_PROGRAM_REDUCER',
-            payload: programResponse.data,
-        })
-    } catch(error){
-        console.log('get program error: ', error);
-    }
-}
+// function* getAccountSaga(action){
+//     try{
+//         const accountResponse = yield call(axios.get, `/api/manage/${action.payload.name}`)
+//         console.log('action payload of getProgramSaga', action.payload);
+//         yield put({
+//             type:'SET_PROGRAM_REDUCER',
+//             payload: accountResponse.data,
+//         })
+//     } catch(error){
+//         console.log('get program error: ', error);
+//     }
+// }
 
-export default manageAccountsSaga;
+// export default manageAccountsSaga;
