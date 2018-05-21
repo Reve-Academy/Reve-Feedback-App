eaf7259cabc58d94c56f65a61e7f1815b7a03027
Melody Massard <melody.massard8988@gmail.com>
2018-05-21 15:55:29 -0500

    sourced modals

--- src/components/Home/AllProgramsPage/Home_AllProgramsPage.js
+++ src/components/Home/AllProgramsPage/Home_AllProgramsPage.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import Modal from '@material-ui/core/Modal';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 import Nav from '../../Nav/Nav';
 
@@ -11,6 +15,30 @@ import { triggerLogout } from '../../../redux/actions/loginActions';
 const mapStateToProps = state => ({
   user: state.user,
 });
+
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50 ;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const styles = theme => ({
+  paper: {
+    position: 'absolute',
+    width: theme.spacing.unit * 50,
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing.unit * 4,
+  },
+});
+
+
 
 class Home_AllProgramsPage extends Component {
   componentDidMount() {
@@ -29,8 +57,17 @@ class Home_AllProgramsPage extends Component {
     this.props.history.push('home');
   }
 
-  render() {
+  handleOpen = () => {
+    this.setState({ open: true });
+  };
 
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
+
+  render() {
+    const { classes } = this.props;
   
     let content = null;
     
@@ -58,7 +95,7 @@ class Home_AllProgramsPage extends Component {
           </div>
 
           <h1>
-            THIS IS THE ALL/MANGE PROGRAMS VIEW
+            ALL PROGRAMS in order of most recent at the top, and deactivated underneath
           </h1>
           <button
             onClick={this.logout}
--- src/components/Home/ManageAccountsPage/ManageAccountsPage.js
+++ src/components/Home/ManageAccountsPage/ManageAccountsPage.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import Modal from '@material-ui/core/Modal';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
 
 import Nav from '../../Nav/Nav';
 
 import { USER_ACTIONS } from '../../../redux/actions/userActions';
 
 
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50 ;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const styles = theme => ({
+  paper: {
+    position: 'absolute',
+    width: theme.spacing.unit * 50,
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing.unit * 4,
+  },
+});
 
 const mapStateToProps = state => ({
   user: state.user,
@@ -23,8 +49,18 @@ class ManageAccountsPage extends Component {
     }
   }
 
+  handleCreateUserDialog = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
 
   render() {
+
+    const { classes } = this.props;
 
     let content = null;
 
@@ -52,8 +88,31 @@ class ManageAccountsPage extends Component {
           </div>
 
           <h1>
-            THIS IS THE MANAGE ACCOUNTS PAGE
+            Manage Accounts
           </h1>
+          <button onClick={this.handleCreateUserDialog}>Create User</button><br/>
+
+          {/* STRETCH GOAL
+          <input placeholder="Search and Add User"></input><button>Search</button> */}
+
+          <table>
+            <thead>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Admin</th>
+              <th>Active</th>
+              <th>Program</th>
+              <th>Delete</th>
+            </thead>
+            <tbody>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tbody>
+          </table>
         
         </div>
       );
