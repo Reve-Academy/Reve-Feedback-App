aa9ac856cfbe56985771d137a86ce787a43b5de5
Kam Kubesh <kube0057@umn.edu>
2018-05-22 11:58:24 -0500

    added put request

--- server/routes/allPrograms.router.js
+++ server/routes/allPrograms.router.js
@@ -2,15 +2,32 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-/**
- * GET route template
- */
+// GET ROUTER FOR PROGRAMS LIST
 router.get('/', (req, res) => {
     const queryText = 'SELECT * FROM program';
     pool.query(queryText)
         .then(result => { res.send(result.rows); })
         .catch(err => {
-            console.log('Error completing GET programs in router', err);
+            console.log('Error completing GET allPrograms in router', err);
+            res.sendStatus(500);
+        });
+});
+
+// PUT FOR EDITING PROGRAMS
+router.put('/:id', (req, res) => {
+    let p = req.body;
+    const queryText = `UPDATE program SET 
+        name = $1,
+        active_program = $2,
+        description = $3,
+        start = $4,
+        finish = $5
+        WHERE id = $6;`;
+    pool.query(queryText, [p.name, p.active_program, p.description, p.start, 
+        p.finish, p.id])
+        .then(result => { res.send(result.rows); })
+        .catch(err => {
+            console.log('Error completing GET allPrograms in router', err);
             res.sendStatus(500);
         });
 });
