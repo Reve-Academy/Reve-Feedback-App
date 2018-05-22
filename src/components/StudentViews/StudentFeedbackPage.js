eb45c17c9033cdd2f3870eeacf3474fd7fdf6b55
Sam Trapskin <samtrapskin@gmail.com>
2018-05-22 00:40:34 -0500

    updated nav styling

--- src/components/StudentViews/StudentFeedbackPage.js
+++ src/components/StudentViews/StudentFeedbackPage.js
@@ -3,76 +3,60 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import StudentNav from '../../components/Nav/StudentNav';
 
-
-
-
 import { USER_ACTIONS } from '../../redux/actions/userActions';
 
-
-
-const mapStateToProps = state => ({
-  user: state.user,
+const mapStateToProps = (state) => ({
+	user: state.user
 });
 
 class StudentFeedbackPage extends Component {
-  componentDidMount() {
-    this.props.dispatch({ type: USER_ACTIONS.FETCH_USER });
-  }
+	componentDidMount() {
+		this.props.dispatch({ type: USER_ACTIONS.FETCH_USER });
+	}
 
-  componentDidUpdate() {
-    if (!this.props.user.isLoading && this.props.user.userName === null) {
-      this.props.history.push('home');
-    }
-  }
+	componentDidUpdate() {
+		if (!this.props.user.isLoading && this.props.user.userName === null) {
+			this.props.history.push('home');
+		}
+	}
 
+	render() {
+		let content = null;
 
-  render() {
+		if (this.props.user.userName && this.props.user.userName.instructor === false) {
+			content = (
+				<div>
+					<div className="navbar">
+						<ul>
+							{/* Reve Academy logo */}
 
-    let content = null;
+							<img className="logo" src="images/academylogo_white.png" alt="Reve logo" />
 
-    if (this.props.user.userName && this.props.user.userName.instructor === false ) {
-      content = (
-        <div>
-          <div className="navbar">
-          <ul>
-          {/* Reve Academy logo */}
-         
-          <img className="logo" src="images/academylogo_white.png" alt="Reve logo" />
-        
-            <li>
-              <Link to="/StudentFeedback">
-                Feedback
-              </Link>
-            </li>
-            <li>
-              <Link to="/StudentSchedule">
-                Schedule
-              </Link>
-            </li>
-          </ul>
-          </div>
+							<li>
+								<Link to="/StudentFeedback">Feedback</Link>
+							</li>
+							<li>
+								<Link to="/StudentSchedule">Schedule</Link>
+							</li>
+						</ul>
+					</div>
 
-          <h1>
-            STUDENT FEEDBACK
-          </h1>
-          
-          {/* Feedback Container */}
-          <div>
-            This is where feedback will be sourced in.
-          </div>
-          {/* End Feedback Container */}
+					<h1>STUDENT FEEDBACK</h1>
 
-        </div>
-      );
-    }
+					{/* Feedback Container */}
+					<div>This is where feedback will be sourced in.</div>
+					{/* End Feedback Container */}
+				</div>
+			);
+		}
 
-    return (
-      <div>
-        <StudentNav />
-        {content}
-      </div>
-    );
-  }
+		return (
+			<div>
+				<StudentNav />
+				{content}
+			</div>
+		);
+	}
 }
 
 // this allows us to use <App /> in index.js
