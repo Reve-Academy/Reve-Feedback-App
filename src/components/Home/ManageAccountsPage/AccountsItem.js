eb4d423d8b3fd2add801970c75ccb3f0fd2570ca
Melody Massard <melody.massard8988@gmail.com>
2018-05-23 13:37:31 -0500

    check boxes added

--- src/components/Home/ManageAccountsPage/AccountsItem.js
+++ src/components/Home/ManageAccountsPage/AccountsItem.js
@@ -2,97 +2,79 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 //import material-ui
-import Button from '@material-ui/core/Button'
-import TextField from '@material-ui/core/TextField';
-import MenuItem from '@material-ui/core/MenuItem';
-import InputLabel from '@material-ui/core/InputLabel';
-import Select from '@material-ui/core/Select';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import Checkbox from '@material-ui/core/Checkbox';
+import CheckBox from '@material-ui/icons/CheckBox';
+import CheckBoxOutlineBlank from '@material-ui/icons/CheckBoxOutlineBlank'
 
+//Recieve from redux
 const mapStateToProps = state => ({
     state,
 });
 
 class AccountsItem extends Component {
     constructor(props) {
-       super(props);
+        super(props);
         this.state = {
-                id: this.props.aItem.id,
-                instructor: this.props.aItem.instructor,
-                active: this.props.aItem.active_profile
+            id: this.props.aItem.id,
+            instructor: this.props.aItem.instructor,
+            active: this.props.aItem.active_profile
         }
     }
 
+    //Dispatch account ACTIVE STATUS
+    handleActive = () => {
+        this.props.dispatch({
+            type: 'UPDATE_ACTIVE_STATUS',
+            payload: this.state
+        })
 
-        handleActive = () => {
-            this.props.dispatch({
-                type: 'UPDATE_ACTIVE_STATUS', 
-                payload: this.state
-            })
+    }
 
+    //Dispatch account INSTRUCTOR STATUS
+    handleInstructor = () => {
+        this.props.dispatch({
+            type: 'UPDATE_ADMIN_STATUS',
+            payload: this.state
+        })
+    }
+
+    render() {
+        //True and false to be displayed as checkboxes
+        let active;
+
+        if (this.props.aItem.active_profile === true) {
+            active = (<CheckBox onClick={() => this.handleActive()}/>)
+
+        } else {
+            active = (<CheckBoxOutlineBlank onClick={() => this.handleActive()}/>)
+        }
+
+        let instructor;
+
+        if (this.props.aItem.instructor === true) {
+            instructor = (<CheckBox onClick={() => this.handleIntructor()}/>)
+
+        } else {
+            instructor = (<CheckBoxOutlineBlank onClick={() => this.handleInstructor()}/>)
         }
 
 
-        handleInstructor = () => {
-            this.props.dispatch({
-                type: 'UPDATE_ADMIN_STATUS', 
-                payload: this.state
-            })
-        }
-
-        render(){
-           
-
-            //True and false to be displayed as checkboxes
-            let active;
-
-            if (this.props.aItem.active_profile === true) {
-                active = (<p>true</p>)
-
-            } else {
-                active = (<p>false</p>)
-            }
-
-            let instructor;
-
-            if (this.props.aItem.instructor === true) {
-                instructor = (<p>true</p>)
-
-            } else {
-                instructor = (<p>false</p>)
-            }
 
 
-
-            return (
-
-
-
-                <tbody>
-                    <tr>
-                        <td>{this.props.aItem.first}</td>
-                        <td>{this.props.aItem.last}</td>
-                        <td onClick={() => this.handleInstructor()}>{instructor}</td>
-                        <td onClick={() => this.handleActive()}>{active}</td>
-                        <td>{this.props.aItem.name}</td>
-                        <td>{this.props.aItem.high_school}</td>
-                        <td>{this.props.aItem.team}</td>
-                        <td><button>Delete</button></td>
-
-                    </tr>
-                </tbody>
-
-
-
-
-            )
-
-
-        }
-    
-
+        return (
+            <tbody>
+                <tr>
+                    <td>{this.props.aItem.first}</td>
+                    <td>{this.props.aItem.last}</td>
+                    <td >{instructor}</td>
+                    <td >{active}</td>
+                    <td>{this.props.aItem.name}</td>
+                    <td>{this.props.aItem.high_school}</td>
+                    <td>{this.props.aItem.team}</td>
+                    <td><button>Delete</button></td>
+                </tr>
+            </tbody>
+        )
+    }
 }
 
 export default connect(mapStateToProps)(AccountsItem)
