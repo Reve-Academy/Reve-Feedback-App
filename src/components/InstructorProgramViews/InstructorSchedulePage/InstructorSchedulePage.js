8d63b308c786eb76ad4eb4ed7ec094a13bd986c0
Ian Carthey <PimpDaddyE@Ians-MacBook-Pro-2.local>
2018-05-23 13:56:47 -0500

    added base of library

--- src/components/InstructorProgramViews/InstructorSchedulePage/InstructorSchedulePage.js
+++ src/components/InstructorProgramViews/InstructorSchedulePage/InstructorSchedulePage.js
@@ -1,16 +1,67 @@
+// react, redux imports
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+
+//library import
+import RGL, { WidthProvider } from 'react-grid-layout';
+import _ from 'lodash';
+
 import InstructorNav from '../../Nav/InstructorNav';
 import { USER_ACTIONS } from '../../../redux/actions/userActions';
 
-
+const ReactGridLayout = WidthProvider(RGL);
 
 const mapStateToProps = state => ({
   user: state.user,
 });
 
 class InstructorSchedulePage extends Component {
+
+  static defaultProps = {
+    className: "layout",
+    items: 10,
+    rowHeight: 20,
+    onLayoutChange: function() {},
+    cols: 8
+  };
+
+  constructor(props) {
+    super(props);
+
+    const layout = this.generateLayout();
+    this.state = { layout };
+  }
+
+  generateDOM() {
+    return _.map(_.range(this.props.items), function(i) {
+      return (
+        <div key={i}>
+          <span className="text">{i}</span>
+        </div>
+      );
+    });
+  }
+
+  generateLayout() {
+    const p = this.props;
+    return _.map(new Array(p.items), function(item, i) {
+      const y = _.result(p, "y") || Math.ceil(Math.random() * 4) + 1;
+      return {
+        x: (i * 2) % 12,
+        y: Math.floor(i / 6) * y,
+        w: 2,
+        h: y,
+        i: i.toString()
+      };
+    });
+  }
+
+  onLayoutChange(layout) {
+    this.props.onLayoutChange(layout);
+  }
+
+
   componentDidMount() {
     this.props.dispatch({ type: USER_ACTIONS.FETCH_USER });
   }
@@ -61,9 +112,13 @@ class InstructorSchedulePage extends Component {
           </h1>
 
           {/* Schedule Container */}
-          <div>
-            This is where schedule will be sourced in.
-          </div>
+            <ReactGridLayout
+            layout={this.state.layout}
+            onLayoutChange={this.onLayoutChange}
+            {...this.props}
+            >
+            {this.generateDOM()}
+            </ReactGridLayout>
           {/* End Schedule Container */}
 
         </div>
