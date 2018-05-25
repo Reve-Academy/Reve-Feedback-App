1a3739b7a4ad913e82584f349c3c9dfe6993707b
Melody Massard <melody.massard8988@gmail.com>
2018-05-25 10:58:50 -0500

    nodemailer sends message on terminal command

---
+++ mysendmail.js
@@ -0,0 +1,49 @@
+const nodemailer = require('nodemailer');
+var xoauth2 = require('xoauth2');
+
+// //Nodemailer
+// var smtpTransport = nodemailer.createTransport("SMTP", {
+//     service: "Gmail",
+//     auth: {
+//       XOAuth2: {
+//         user: "melody.massard8988@gmail.com", // Your gmail address.
+//                                               // Not @developer.gserviceaccount.com
+//         clientId: "854064104990-tc8sdduk70trjlj27g16vpk6te595ed2.apps.googleusercontent.com",
+//         clientSecret: "NohG1b8S4myFGL5Wqx236WC8",
+//         refreshToken: "1/v1sfUNC0_2J26yR1exXcRTnYlM6kmLko_xs4p5VdvM0"//"expires_in": 3600, 
+//       }
+//     }
+//   });
+
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+       
+            user: 'melody.massard8988@gmail.com',
+            type: 'OAuth2',
+            clientId: '854064104990-tc8sdduk70trjlj27g16vpk6te595ed2.apps.googleusercontent.com',
+            clientSecret: 'NohG1b8S4myFGL5Wqx236WC8',
+            refreshToken: '1/UXv9T6L5VMVfVd17Mn7Oi4S6ybd37nv9z_TJHQLxmY8',
+            accessToken: 'ya29.GlvGBUH2GtUvq9PUFWZfXOsWCsuiqS7n8tS191EmdloMFetybcNGNcWfYCWYVehYhvLtEZjghmRWCxZfRxPO8BjDbdCeDaQOsj6LG7cK5dwnbLMawrodJ1PpqrZz'
+       
+    }
+});
+  
+  let mailOptions = {
+    from: "melody.massard8988@gmail.com",
+    to: "kamkubesh@gmail.com",
+    subject: "Hello",
+    generateTextFromHTML: true,
+    html: "<b>Hello world</b>"
+  };
+  
+  transporter.sendMail(mailOptions, function(error, response) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(response);
+    }
+    transporter.close();
+  });
+  //End Nodemailer code
+
