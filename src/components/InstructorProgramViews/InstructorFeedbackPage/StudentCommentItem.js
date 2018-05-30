f98427e059dcf464a48eb8ac94b1605175c53471
Tenzin Chosang <TenzinChosang@Tenzins-MacBook-Air.local>
2018-05-30 11:29:17 -0500

    styling feedbackpage

--- src/components/InstructorProgramViews/InstructorFeedbackPage/StudentCommentItem.js
+++ src/components/InstructorProgramViews/InstructorFeedbackPage/StudentCommentItem.js
@@ -4,6 +4,7 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Modal from '@material-ui/core/Modal';
 import { withStyles } from '@material-ui/core/styles';
+import star from '../styles/images/star.png';
 
 const mapStateToProps = (state) => ({
 	state
@@ -11,7 +12,7 @@ const mapStateToProps = (state) => ({
 
 const styles = theme => ({
     paper: {
-      position: 'absolute',
+      position: 'relative',
       width: theme.spacing.unit * 50,
       backgroundColor: theme.palette.background.paper,
       boxShadow: theme.shadows[5],
@@ -31,11 +32,14 @@ class StudentComment extends Component {
     
     render(){
         return(
-            <Card>
-                <CardContent>
-                    <div> Author: {this.props.comment.first} {this.props.comment.last} <br/> <h2>{this.props.comment.comment}</h2></div> <br/> <button onClick={this.deleteComment}>delete</button> 
-                </CardContent>
-            </Card>
+            <div>
+                <img className="star" src={star}/>
+                <Card style={itemStyle.feedbackField}>
+                    <CardContent>
+                        <div> Author: {this.props.comment.first} {this.props.comment.last} <br/> <h2>{this.props.comment.comment}</h2></div> <br/> <button onClick={this.deleteComment}>delete</button> 
+                    </CardContent>
+                </Card>
+            </div>
         )
     }
 }
