850c0e1fd35dc3fbc66a8bc4ed13a7d1c329c93c
Melody Massard <melody.massard8988@gmail.com>
2018-06-06 14:15:06 -0500

    deploy reve voices to heroku

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -4,14 +4,13 @@ OUR DATABASE NAME: 'reveAcademy'
 
 const pg = require('pg');
 const url = require('url');
-
 let config = {};
 
 if (process.env.DATABASE_URL) {
   // Heroku gives a url, not a connection object
   // https://github.com/brianc/node-pg-pool
-  const params = url.parse(process.env.DATABASE_URL);
-  const auth = params.auth.split(':');
+  let params = url.parse(process.env.DATABASE_URL);
+  let auth = params.auth.split(':');
 
   config = {
     user: auth[0],
@@ -23,29 +22,18 @@ if (process.env.DATABASE_URL) {
     max: 10, // max number of clients in the pool
     idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
   };
+
 } else {
+  // only change the things on the right side of the ||
   config = {
-    host: 'localhost', // Server hosting the postgres database
-    port: 5432, // env var: PGPORT
-    database: 'reveAcademy', // CHANGE THIS LINE! env var: PGDATABASE, this is likely the one thing you need to change to get up and running
+    user: process.env.PG_USER || null, //env var: PGUSER
+    password: process.env.DATABASE_SECRET || null, //env var: PGPASSWORD
+    host: process.env.DATABASE_SERVER || 'localhost', // Server hosting the postgres database
+    port: process.env.DATABASE_PORT || 5432, //env var: PGPORT
+    database: process.env.DATABASE_NAME || 'reveAcademy', //env var: PGDATABASE or the name of your database (e.g. database: process.env.DATABASE_NAME || 'koala_holla',)
     max: 10, // max number of clients in the pool
     idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
   };
 }
 
-// this creates the pool that will be shared by all other modules
-const pool = new pg.Pool(config);
-
-// the pool will log when it connects to the database
-pool.on('connect', () => {
-  console.log('Postgesql connected');
-});
-
-// the pool with emit an error on behalf of any idle clients
-// it contains if a backend error or network partition happens
-pool.on('error', (err) => {
-  console.log('Unexpected error on idle client', err);
-  process.exit(-1);
-});
-
-module.exports = pool;
+module.exports = new pg.Pool(config);
